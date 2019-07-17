b87fbb750059de6a961d4bdc914e78c06752a69d
Perluceo <FullStackDevSMU@gmail.com>
2019-07-17 11:26:36 -0500

    module.export in data table

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,15 +1,17 @@
 //list of intial matches
 var matches = [
 	{
-		"name": "Brandon",
-		"photo": "https://format-com-cld-res.cloudinary.com/image/private/s--WgLmCKTi--/c_limit,g_center,h_700,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/dd6e24cdfe7cfe835ee6e6701fe8979e/C-0903_web.jpg",
-		"scores": [1,1,1,1,1,1,1,1,1,1]
+		name: "Brandon",
+		photo: "https://format-com-cld-res.cloudinary.com/image/private/s--WgLmCKTi--/c_limit,g_center,h_700,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/dd6e24cdfe7cfe835ee6e6701fe8979e/C-0903_web.jpg",
+		scores: ["1","1","1","1","1","1","1","1","1","1"]
 		//10
 	},
 	{
-		"name": "Robert",
-		"photo": "https://static1.squarespace.com/static/57809f1fe6f2e12e1da3e283/57831f13ff7c501754f8277e/5783ed51b3db2bae65a7255a/1525374811155/Headshots_Men88.jpg?format=300w",
-		"scores": [5,5,5,5,5,5,5,5,5,5]
+		name: "Robert",
+		photo: "https://static1.squarespace.com/static/57809f1fe6f2e12e1da3e283/57831f13ff7c501754f8277e/5783ed51b3db2bae65a7255a/1525374811155/Headshots_Men88.jpg?format=300w",
+		scores: ["5","5","5","5","5","5","5","5","5","5"]
 		//55		
 	}
 ];
+
+module.export = matches;
