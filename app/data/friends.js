c72072154d81b97eaee12e22631c2f3c257a1689
Perluceo <FullStackDevSMU@gmail.com>
2019-07-17 16:38:41 -0500

    refined survey questions and added more friend possibilities

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,16 +1,28 @@
 //list of intial matches
 var matchList = [
 	{
-		name: "Brandon",
-		photo: "https://format-com-cld-res.cloudinary.com/image/private/s--WgLmCKTi--/c_limit,g_center,h_700,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/dd6e24cdfe7cfe835ee6e6701fe8979e/C-0903_web.jpg",
-		scores: ["1","1","1","1","1","1","1","1","1","1"]
-		//10
+		name: "Alex",
+		photo: "https://i.pinimg.com/236x/a9/9c/11/a99c11c8501eaf5312db9ac4cd528d56--famous-armenians-actor-headshots.jpg",
+		scores: ["4","4","4","4","4","4","4","4","4","4"]
+		//40
+	},
+	{
+		name: "Kensie",
+		photo: "https://format-com-cld-res.cloudinary.com/image/private/s--Cs4gibMJ--/c_limit,g_center,h_65535,w_700/a_auto,fl_keep_iptc.progressive,q_95/v1/e013576a1a2d5532f9e7748f6490f08b/Kaitlyn-Sapp414.jpg",
+		scores: ["5","5","5","5","5","5","5","5","5","5"]
+		//50		
 	},
 	{
 		name: "Robert",
 		photo: "https://static1.squarespace.com/static/57809f1fe6f2e12e1da3e283/57831f13ff7c501754f8277e/5783ed51b3db2bae65a7255a/1525374811155/Headshots_Men88.jpg?format=300w",
-		scores: ["5","5","5","5","5","5","5","5","5","5"]
-		//55		
+		scores: ["2","2","2","2","2","2","2","2","2","2"]
+		//20		
+	},
+	{
+		name: "Brandon",
+		photo: "https://format-com-cld-res.cloudinary.com/image/private/s--WgLmCKTi--/c_limit,g_center,h_700,w_65535/a_auto,fl_keep_iptc.progressive,q_95/v1/dd6e24cdfe7cfe835ee6e6701fe8979e/C-0903_web.jpg",
+		scores: ["1","1","1","1","1","1","1","1","1","1"]
+		//10		
 	}
 ];
 
